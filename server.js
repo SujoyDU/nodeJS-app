f10572172461778dd3cbd64aae97d26d9b61b676
sujoyDU <sujoydebnath.92@gmail.com>
2019-01-07 21:05:42 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var socket = require('socket.io');
+var app = express();
+
+var server = app.listen(3000);
+app.use(express.static('public'));
+
+
+console.log("My server is running");
+
+var io = socket(server);
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log('new connection ' + socket.id)
+        // console.log(socket)
+}
