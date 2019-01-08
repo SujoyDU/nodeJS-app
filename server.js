8f9516d07ca13aa7a96e0072f64020be467a595e
sujoyDU <sujoydebnath.92@gmail.com>
2019-01-08 10:37:44 -0500

    two apps connected

--- server.js
+++ server.js
@@ -3,15 +3,23 @@ var socket = require('socket.io');
 var app = express();
 
 var server = app.listen(3000);
-app.use(express.static('public'));
-
+app.use('/app1', express.static('public'))
+app.use('/app2', express.static('public2'))
 
 console.log("My server is running");
-
 var io = socket(server);
-io.sockets.on('connection', newConnection);
+io.on('connection', newConnection);
 
 function newConnection(socket) {
     console.log('new connection ' + socket.id)
-        // console.log(socket)
+    console.log('connected:', socket.client.id);
+    socket.on('mouse', function(data) {
+        console.log('new message from client:', data);
+        socket.broadcast.emit('mouse', data)
+    });
+    // setInterval(function() {
+    //     socket.emit('clientEvent', Math.random());
+    //     console.log('message sent to the clients');
+    // }, 3000);
+    // console.log(socket)
 }
