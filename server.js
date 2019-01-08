30400b58c83ea8f653511c9d63456d8fcd7772bc
sujoyDU <sujoydebnath.92@gmail.com>
2019-01-08 12:39:14 -0500

    app a is done

--- server.js
+++ server.js
@@ -9,12 +9,27 @@ app.use('/app2', express.static('public2'))
 console.log("My server is running");
 var io = socket(server);
 io.on('connection', newConnection);
+const fs = require('fs');
+fs.open('mouselog.txt', 'w', function(err, file) {
+    if (err) throw err;
+    console.log('Saved!');
+});
+
 
 function newConnection(socket) {
+
+    // append_file.js
+    // add a line to a lyric file, using appendFile
+
     console.log('new connection ' + socket.id)
     console.log('connected:', socket.client.id);
     socket.on('mouse', function(data) {
         console.log('new message from client:', data);
+
+        fs.appendFile('mouselog.txt', data.x + ' ' + data.y + ' ' + data.time + '\n', (err) => {
+            if (err) throw err;
+            console.log('The data were updated!');
+        });
         socket.broadcast.emit('mouse', data)
     });
     // setInterval(function() {
