a7ce2a9cbae4ed3db71f325a3e6435aeb3018651
sujoyDU <sujoydebnath.92@gmail.com>
2019-01-09 11:52:40 -0500

    refactored the code

--- server.js
+++ server.js
@@ -1,40 +1,39 @@
 var express = require('express');
 var socket = require('socket.io');
 var app = express();
+var port = 3000;
+var server = app.listen(port);
+// server runs at https://localhost:3000
 
-var server = app.listen(3000);
-app.use('/app1', express.static('public'))
-app.use('/app2', express.static('public2'))
+//App A is is in AppA Folder, App B is in AppB folder 
+app.use('/app1', express.static('AppA'))
+app.use('/app2', express.static('AppB'))
+console.log("Server is running at port " + port);
 
-console.log("My server is running");
+//initialize websocket with socket.io
 var io = socket(server);
 io.on('connection', newConnection);
+
+//create a new text file called "mouselog.txt" for storing client data from App A
 const fs = require('fs');
 fs.open('mouselog.txt', 'w', function(err, file) {
     if (err) throw err;
-    console.log('Saved!');
+    console.log('Text File Created!');
 });
 
 
 function newConnection(socket) {
-
-    // append_file.js
-    // add a line to a lyric file, using appendFile
-
+    //showing new connection id 
     console.log('new connection ' + socket.id)
-    console.log('connected:', socket.client.id);
-    socket.on('mouse', function(data) {
-        console.log('new message from client:', data);
+
+    //writing data to text file and broadcasting data to App B
+    socket.on('mouseMovePoint', function(data) {
+        console.log('new message from App A:', data);
 
         fs.appendFile('mouselog.txt', data.x + ' ' + data.y + ' ' + data.time + '\n', (err) => {
             if (err) throw err;
             console.log('The data were updated!');
         });
-        socket.broadcast.emit('mouse', data)
+        socket.broadcast.emit('mouseMovePoint', data)
     });
-    // setInterval(function() {
-    //     socket.emit('clientEvent', Math.random());
-    //     console.log('message sent to the clients');
-    // }, 3000);
-    // console.log(socket)
 }
